be9b32cc76aa51b937ffc754a13a602ffbd0d4fd
Roman Shtemberko <roman.shtemberko@globallogic.com>
2016-08-03 16:03:48 +0300

    Add linking script for Ubuntu.

---
+++ bubuntu/LINK.sh
@@ -0,0 +1,14 @@
+base_dir=$(pwd)
+target_dir=~
+this_filename=`basename "$0"`
+
+
+cd $target_dir
+
+# create folders if missing
+find $base_dir -mindepth 1 -depth -type d -printf "%P\n" | while read dir; do mkdir -p "$dir"; done
+
+# link files (except this one)
+find $base_dir -type f \( -iname "*" ! -iname "*$this_filename*" \) -printf "%P\n" | while read file; do ln -s "$base_dir/$file" "$file"; done
+
+cd $target_dir
